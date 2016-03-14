664c08f4ba88e64d1fc6cce663bcfcf96e6180b0
wǒ_is神仙 <i@mrzhang.me>
2016-03-14 18:04:02 +0800

    Add controllers and views

---
+++ ios-learning/Controllers/Base/TabBarController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.initViewControllers()
+    }
+
+    func initViewControllers() {
+
+        let homeNavController = UINavigationController(rootViewController: HomeViewController())
+        homeNavController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "Home"), selectedImage: UIImage(named: "Home-selected"))
+
+        let webviewNavController = UINavigationController(rootViewController: WebViewController())
+        webviewNavController.tabBarItem = UITabBarItem(title: "Webview", image: UIImage(named: "Puzzle"), selectedImage: UIImage(named: "Puzzle-selected"))
+        webviewNavController.tabBarItem.title = "Webview"
+
+        self.tabBar.tintColor = UIColor(hex: "#f00")
+        self.viewControllers = [homeNavController, webviewNavController]
+
+    }
+
+}
---
+++ ios-learning/Views/HomeViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+import WebKit
+
+class HomeViewController: UIViewController {
+    
+    // 设置顶部状态栏颜色
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let source = "document.body.style.paddingTop = '22px';document.body.style.backgroundColor = '#f63';"
+//        let userScript = WKUserScript(source: source, injectionTime: .AtDocumentEnd, forMainFrameOnly: false)
+        
+        let userContentController = WKUserContentController()
+//        userContentController.addUserScript(userScript)
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = userContentController
+        
+        
+        
+        let w = self.view.frame.size.width
+        let h = self.view.frame.size.height// - 49
+        
+        let webview = WKWebView(frame: CGRect(x: 0, y: 0, width: w, height: h), configuration: config)
+        
+        let url = NSURL(string: "http://m.dianping.com/")
+        webview.loadRequest(NSURLRequest(URL: url!))
+        
+        self.view = webview
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
