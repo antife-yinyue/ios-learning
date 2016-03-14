b4365153ff28b0a56c33119fd96dc44788ebd692
wǒ_is神仙 <i@mrzhang.me>
2016-03-14 18:05:56 +0800

    Set root view controller

--- ios-learning/AppDelegate.swift
+++ ios-learning/AppDelegate.swift
@@ -8,6 +8,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        self.window?.rootViewController = TabBarController()
+        self.window?.backgroundColor = UIColor(hex: "#fff")
+
         return true
     }
 
